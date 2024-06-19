000a1e746e0d195fa748fd28b5dd6d11df5b92a4
Ahmed-elsaid-AEA <ahmed122727727@gmail.com>
2024-06-19 21:08:40 +0300

    make animation to custom splash screen

--- lib/view/splash_screen/screens/splash_screen.dart
+++ lib/view/splash_screen/screens/splash_screen.dart
@@ -1,33 +1,76 @@
 import 'package:drosak_m_app/core/resources/assets_values_mananger.dart';
 import 'package:drosak_m_app/core/resources/colors_manager.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:drosak_m_app/core/resources/routes_mananger.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _animationBottom;
+  late Animation<Offset> _animationTop;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          Navigator.pushReplacementNamed(context, RoutesName.kOnBoardingScreen);
+        }
+      });
+    _animationBottom = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _animationTop = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: Offset.zero,
+    ).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ColorManager.kBlackColor,
-
-        body: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              Image.asset(AssetsValuesManager.kSplashBorderImage),
-              Align(child: Image.asset(AssetsValuesManager.kLogoImage)),
-              Align(
-                  alignment: AlignmentDirectional.bottomEnd,
-                  child: Image.asset(AssetsValuesManager.kSplashBorderImageBottom)),
-
-            ],
-          ),
-
+      backgroundColor: ColorManager.kBlackColor,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SlideTransition(
+              position: _animationTop,
+              child: Image.asset(AssetsValuesManager.kSplashBorderImage),
+            ),
+            Align(child: Image.asset(AssetsValuesManager.kLogoImage)),
+            Align(
+                alignment: AlignmentDirectional.bottomEnd,
+                child: SlideTransition(
+                    position: _animationBottom,
+                    child: Image.asset(
+                        AssetsValuesManager.kSplashBorderImageBottom))),
+          ],
         ),
-        );
+      ),
+    );
   }
 }

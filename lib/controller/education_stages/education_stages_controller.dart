6f91a734bc1fa08037dfd4952c5b4fb9a2800fbd
Ahmed-elsaid-AEA <ahmed122727727@gmail.com>
2024-07-02 14:00:37 +0300

    finish bottom model sheet

--- lib/controller/education_stages/education_stages_controller.dart
+++ lib/controller/education_stages/education_stages_controller.dart
@@ -1,59 +1,20 @@
-import 'package:drosak_m_app/core/resources/assets_values_mananger.dart';
-import 'package:drosak_m_app/core/resources/colors_manager.dart';
-import 'package:drosak_m_app/core/resources/const_values.dart';
-import 'package:drosak_m_app/core/resources/fonts_manager.dart';
-import 'package:drosak_m_app/core/resources/height_manager.dart';
-import 'package:drosak_m_app/core/resources/padding_manager.dart';
-import 'package:drosak_m_app/core/resources/radius_values_manager.dart';
-import 'package:drosak_m_app/core/resources/width_manager.dart';
-import 'package:drosak_m_app/core/widgets/input_field/custom_text_form_field.dart';
-import 'package:drosak_m_app/core/widgets/space/horizontal_space.dart';
-import 'package:drosak_m_app/core/widgets/space/vertical_space.dart';
 import 'package:drosak_m_app/model/education_stages/item_stage_model.dart';
+import 'package:drosak_m_app/view/education_stages/widgets/body/custom_add_new_eudcation_stage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class EducationStagesController {
   List<ItemStageModel> listItemStageModel = [];
+  TextEditingController controllerAddNewEducationalStage =
+      TextEditingController();
 
   void openBottomSheet({required BuildContext context}) {
     showModalBottomSheet(
       context: context,
-      builder: (context) => Directionality(
-        textDirection: TextDirection.rtl,
-        child: Container(
-          padding: EdgeInsets.symmetric(
-              horizontal: PaddingManager.pw12, vertical: PaddingManager.ph22),
-          decoration: BoxDecoration(
-              color: ColorManager.kBlackLight,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(RadiusValuesManager.br12),
-                topRight: Radius.circular(RadiusValuesManager.br12),
-              )),
-          width: double.infinity,
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: CustomTextFormField(),
-                  ),
-                  HorizontalSpace(WidthManager.w6),
-                  IconButton.filled(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: ColorManager.kPrimaryColor),
-                      icon:
-                          SvgPicture.asset(AssetsValuesManager.kPlaceholderSvg))
-                ],
-              ),
-              VerticalSpace(HeightManager.h40),
-
-            ],
-          ),
-        ),
-      ),
+      builder: (context) => CustomAddNewEducationStage(
+          controllerAddNewEducationalStage: controllerAddNewEducationalStage),
     );
   }
-}
+  void addToNewEducationalStageToDatabase(){
 
+  }
+}
---
+++ lib/view/education_stages/widgets/body/custom_add_new_eudcation_stage.dart
@@ -0,0 +1,64 @@
+import 'package:drosak_m_app/core/resources/assets_values_mananger.dart';
+import 'package:drosak_m_app/core/resources/colors_manager.dart';
+import 'package:drosak_m_app/core/resources/const_values.dart';
+import 'package:drosak_m_app/core/resources/height_manager.dart';
+import 'package:drosak_m_app/core/resources/padding_manager.dart';
+import 'package:drosak_m_app/core/resources/radius_values_manager.dart';
+import 'package:drosak_m_app/core/resources/width_manager.dart';
+import 'package:drosak_m_app/core/widgets/buttons/custom_material_button.dart';
+import 'package:drosak_m_app/core/widgets/input_field/custom_text_form_field.dart';
+import 'package:drosak_m_app/core/widgets/space/horizontal_space.dart';
+import 'package:drosak_m_app/core/widgets/space/vertical_space.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomAddNewEducationStage extends StatelessWidget {
+  const CustomAddNewEducationStage({
+    super.key,
+    required this.controllerAddNewEducationalStage,
+  });
+
+  final TextEditingController controllerAddNewEducationalStage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: PaddingManager.pw12, vertical: PaddingManager.ph22),
+        decoration: BoxDecoration(
+            color: ColorManager.kBlackLight,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(RadiusValuesManager.br12),
+              topRight: Radius.circular(RadiusValuesManager.br12),
+            )),
+        width: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    controller: controllerAddNewEducationalStage,
+                    hintText: ConstValue.kNameEducationalStages,
+                  ),
+                ),
+                HorizontalSpace(WidthManager.w6),
+                IconButton.filled(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: ColorManager.kPrimaryColor),
+                    icon:
+                        SvgPicture.asset(AssetsValuesManager.kPlaceholderSvg)),
+              ],
+            ),
+            VerticalSpace(HeightManager.h40),
+            CustomMaterialButton(onPressed: () {}, text: ConstValue.kAdd)
+          ],
+        ),
+      ),
+    );
+  }
+}
